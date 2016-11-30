050fbf73940101d7b49a66666c2d1a0f3377e807
Khaja Minhajuddin <minhajuddin@cosmicvent.com>
2016-11-30 23:11:56 +0530

    added a wercker config

--- mix.exs
+++ mix.exs
@@ -30,14 +30,23 @@ defmodule Danny.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.1"},
-     {:phoenix_pubsub, "~> 1.0"},
-     {:phoenix_ecto, "~> 3.0"},
-     {:postgrex, ">= 0.0.0"},
-     {:phoenix_html, "~> 2.6"},
-     {:phoenix_live_reload, "~> 1.0", only: :dev},
-     {:gettext, "~> 0.11"},
-     {:cowboy, "~> 1.0"}]
+    [
+      {:phoenix, "~> 1.2.1"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:postgrex, ">= 0.0.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:gettext, "~> 0.11"},
+      {:cowboy, "~> 1.0"},
+
+      # deployment stuff
+      {:distillery, "~> 0.10.1"},
+
+      {:phoenix_live_reload, "~> 1.0", only: :dev},
+      {:credo, "~> 0.4", only: [:dev, :test]},
+      {:dogma, "~> 0.1", only: :dev},
+      {:dialyxir, "~> 0.3.3", only: [:dev, :test]},
+   ]
   end
 
   # Aliases are shortcuts or tasks specific to the current project.
